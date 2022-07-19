75f80c719c00ea844eb4ab9c760cf113927eabc1
Fadin Alexey <patriot-47@mail.ru>
2022-07-20 00:19:59 +0400

    feat: add all pages and filmCard component, create mook films

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,25 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const HeadFilm = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drame',
+  yearOfIssue: '2014',
+  img: {
+    src: 'img/the-grand-budapest-hotel-poster.jpg',
+    width: '218',
+    height: '327',
+  }
+};
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={HeadFilm.title}
+      genre={HeadFilm.genre}
+      yearOfIssue={HeadFilm.yearOfIssue}
+      img={HeadFilm.img}
+    />
   </React.StrictMode>,
 );
